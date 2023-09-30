adb07d5b96d5a07cc872697a437f183c18e82d9b
konos93 <konos_mk93@hotmail.com>
2023-09-30 18:05:35 +0300

    Update a.py

--- a.py
+++ a.py
@@ -1,32 +1,26 @@
-import os
 import subprocess
 
-def split_file(input_file, output_pattern):
-    # Create the output directory if it doesn't exist
-    os.makedirs(os.path.dirname(output_pattern), exist_ok=True)
+# Input MP3 file
+input_file = 'file.mp3'  # Replace with your input MP3 file
 
-    # Command to split the file using FFmpeg
-    command = [
-        'ffmpeg',
-        '-i', input_file,
-        '-c', 'copy',
-        '-f', 'segment',
-        '-segment_time', '3540',  # 59 minutes = 3540 seconds
-        '-reset_timestamps', '1',
-        '-map', '0',
-        output_pattern
-    ]
+# Define the desired segment duration (59 minutes) in seconds
+segment_duration = 59 * 60
 
-    # Execute the FFmpeg command
-    subprocess.run(command)
+# Define the output pattern for segment files
+output_pattern = 'segment_%03d.mp3'
 
-# Get the path of the Python script
-script_path = os.path.dirname(os.path.abspath(__file__))
+# Use FFmpeg to create the segments
+command = [
+    'ffmpeg',
+    '-i', input_file,
+    '-c', 'copy',
+    '-f', 'segment',
+    '-segment_time', str(segment_duration),  # 59 minutes = 3540 seconds
+    '-reset_timestamps', '1',
+    '-map', '0',
+    output_pattern
+]
 
-# Input file path (assuming it's in the same folder as the script)
-input_file = os.path.join(script_path, 'file.mp3')
+subprocess.run(command, check=True)
 
-# Output file pattern (e.g., output/output_001.mp3, output/output_002.mp3, ...)
-output_pattern = os.path.join(script_path, 'output', 'output_%03d.mp3')
-
-split_file(input_file, output_pattern)
+print("Segments created successfully.")
